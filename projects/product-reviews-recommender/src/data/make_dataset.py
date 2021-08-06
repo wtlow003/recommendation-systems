25662c3eb41cbc25c785bf155bea901bd6995fa9
wtlow003 <wtlow003@suss.edu.sg>
2021-08-07 02:22:40 +0800

    chore: :art: shift responsiblity of fetching data to `src/prepare.py`

--- projects/product-reviews-recommender/src/data/make_dataset.py
+++ projects/product-reviews-recommender/src/data/make_dataset.py
@@ -1,65 +1,18 @@
-# -*- coding: utf-8 -*-
-import click
-import glob
-import logging
+import ast
 import pandas as pd
-import sys
-import ujson
 
-from datetime import datetime
-from pathlib import Path
 from tqdm import tqdm
 
 
-def _set_logger(log_path):
-    """Setting logger for logging code execution.
-    Args:
-        log_path [str]: eg: "../log/train.log"
-    """
-    log_path = Path(log_path)
-    log_path.parent.mkdir(parents=True, exist_ok=True)
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
-    file_handler = logging.FileHandler(log_path, mode="w")
-    stream_handler = logging.StreamHandler(sys.stdout)
-    formatter = logging.Formatter(
-        "%(asctime)s : %(levelname)s : %(name)s : %(message)s"
-    )
-    file_handler.setFormatter(formatter)
-    stream_handler.setFormatter(formatter)
-
-    logger.addHandler(file_handler)
-    logger.addHandler(stream_handler)
-
-    logger.info("Finished logger configuration!")
-
-    return logger
-
-
-def _read_json(json_path):
-    """Read json file into dataframe.
-
-        Args:
-            json [str]: Path to json dataset.
-        Returns:
-            df [pd.DataFrame]: Json file loaded into dataframe.
-    """
-
-    with open(json_path, "rb") as f:
-        data = f.readlines()
-        data = [ujson.loads(line.strip()) for line in data]
-
-    return data
-
-
-def _calculate_sparsity(df):
-    """[summary]
+def calculate_sparsity(df):
+    """Calculate the data sparsity based on ratings and reviews.
 
     Args:
-        df ([type]): [description]
+        df ([pd.DataFrame]): DataFrame with counts of `overall` and `reviewText`
+            measured against total `reviewerID` * `asin`.
 
     Returns:
-        [type]: [description]
+        [tuple]: Tuple of data sparsity wrt. ratings (`overall`) and reviews (`reviewText`).
     """
     # no. of ratings
     rating_numerator = df["overall"].count()
@@ -77,68 +30,42 @@ def _calculate_sparsity(df):
     return rating_sparsity, review_sparsity
 
 
-@click.command()
-@click.argument("input_filepath", type=str, default="data/raw")
-@click.argument("output_filepath", type=str, default="data/interim/")
-def main(input_filepath, output_filepath):
-    """Runs data processing scripts to turn raw data from (../raw) into
-        transformed data ready for further processing (saved in ../interim).
+def _parse_json(json):
+    """Parse json into generator.
 
-        This functions transforms raw data from json files into merged csv from
-        respective categories' reviews and metadata files.
+    Note: Instead of using json.loads(json), `ast.literal_evcal()` is used
+    to prevent errors from occuring when loading metadata json as the json file
+    is not saved in json structure defined in python e.g., '' used instead of ""
+    for property. Hence, overall performance is slightly slower when loading
+    the json files. If metadata is not required, `ujson.loads()` is preferred to
+    load the json instead.
 
     Args:
-        input_filepath [(str)]: Path to the raw data.
-        ouput_filepath ([str]): Path to store the interim data.
+        json ([str]): Path to json file e.g., '../data/raw/*.json'.
+
+    Returns:
+        [generator]: Generator yielding json data.
     """
-    logger = _set_logger(
-        f"log/make_dataset/make-dataset-{datetime.today().strftime('%b-%d-%Y')}"
-    )
-    logger.info("Loading raw reviews and metadata into merged data.")
-
-    # loading and unloading paths
-    input_filepath, output_filepath = Path(input_filepath), Path(output_filepath)
-
-    # sorting ensures corresponding index between reviews and metadata of categories
-    reviews_jsons = sorted([f for f in glob.glob(f"{input_filepath /'*_5.json'}")])
-    # meta_jsons = sorted([f for f in glob.glob(f"{input_filepath /'meta_*.json'}")])
-    cat_names = [str(name).split("/")[-1].split(".")[0] for name in reviews_jsons]
-    logger.info(f"Reviews jsons: {reviews_jsons}")
-    # logger.info(f"Meta jsons: {meta_jsons}")
-
-    for i in tqdm(range(len(reviews_jsons))):
-        logger.info("Reading raw reviews and metadata into dataframes.")
-        reviews = pd.DataFrame.from_dict(_read_json(f"{reviews_jsons[i]}"))
-        # meta = pd.DataFrame.from_dict(_read_json(f"{meta_jsons[i]}"))
-
-        # selecting relevant columns only
-        reviews_cols_to_keep = ["overall", "reviewerID", "asin", "reviewText"]
-        reviews = reviews[reviews_cols_to_keep]
-        logger.info(
-            f"Num ratings: {reviews['overall'].count()}, "
-            f"reviews: {reviews['reviewText'].count()}, "
-            f"users: {reviews['reviewerID'].nunique()}, "
-            f"items: {reviews['asin'].nunique()}"
-        )
-
-        # meta_cols_to_keep = ["title", "brand", "asin"]
-        # meta = meta[meta_cols_to_keep]
-        # # ref: https://colab.research.google.com/drive/1Zv6MARGQcrBbLHyjPVVMZVnRWsRnVMpV
-        # # removing unformatted title (i.e. some 'title' may still contain html style content)
-        # meta = meta[~meta.title.str.contains("getTime", na=False)]
-
-        # dataframe overview
-        logger.info(reviews.info(memory_usage="deep"))
-        # logger.info(meta.info(memory_usage="deep"))
-
-        # logging sparsity
-        rating_sparsity, review_sparsity = _calculate_sparsity(reviews)
-        logger.info(f"Sparsity: {rating_sparsity} rating, {review_sparsity} review.")
-
-        # saving dataframes to output path
-        reviews.to_csv(output_filepath / f"{cat_names[i]}.csv", index=False)
-        # meta.to_csv(output_filepath / f"meta_{cat_names[i]}.csv", index=False)
+    with open(json) as f:
+        data = f.readlines()
+        for l in tqdm(data):
+            yield ast.literal_eval(l)
 
 
-if __name__ == "__main__":
-    main()
+def json_to_df(json):
+    """Parsing json into DataFrame.
+
+    Args:
+        json ([str]): Path to json file e.g., '../data/raw/*.json'.
+
+    Returns:
+        [pd.DataFrame]: DataFrame containing parsed json file.
+    """
+    i = 0
+    df = {}
+    for review in _parse_json(json):
+        df[i] = review
+        i += 1
+
+    return pd.DataFrame.from_dict(df, orient="index")
+
