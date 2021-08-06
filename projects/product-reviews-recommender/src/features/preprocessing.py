06312ca2b691eca89b274b636f503f81f0733a98
wtlow003 <wtlow003@suss.edu.sg>
2021-08-07 02:37:16 +0800

    chore: :fire: remove redundant code

--- projects/product-reviews-recommender/src/features/preprocessing.py
+++ projects/product-reviews-recommender/src/features/preprocessing.py
@@ -4,20 +4,6 @@ import pandas as pd
 from gensim.models.doc2vec import TaggedDocument
 
 
-def filter_min_length(df, target_col, threshold=100):
-    """Filter target column based on minimum threshold in length.
-
-        Args:
-            df [pd.DataFrame]:
-            target_col [str]:
-        Returns:
-            filtered_df [pd.DataFrame]:
-    """
-
-    df.dropna(axis=0, subset=[target_col], inplace=True)
-    return df[df[target_col].map(len) > 100]
-
-
 def compute_mean_ratings(df, count_col, rating_col):
     """[summary]
 
@@ -40,19 +26,19 @@ def compute_mean_ratings(df, count_col, rating_col):
 def compute_weighted_ratings(df, count_col, avg_rating_col, threshold=0.75):
     """Computes weighted ratings based on number of ratings and average rating.
 
-        Args:
-            df [pd.DataFrame]: DataFrame consisting both rating counts and average rating.
-            count_col [str]: Column name for number of ratings.
-            avg_rating_col [str]: Column name for average rating.
-            threshold [float]: Threshold for minimum of ratings/reviews to qualify for ranking.
-        Returns:
-            df [pd.DataFrame]: DataFrame with newly computed weighted ratings
+    Args:
+        df [pd.DataFrame]: DataFrame consisting both rating counts and average rating.
+        count_col [str]: Column name for number of ratings.
+        avg_rating_col [str]: Column name for average rating.
+        threshold [float]: Threshold for minimum of ratings/reviews to qualify for ranking.
+    Returns:
+        df [pd.DataFrame]: DataFrame with newly computed weighted ratings
     """
 
     m = df[count_col].quantile(threshold)
     C = df[avg_rating_col].mean()  # global average rating
 
-    df["rating_weighted"] = df.apply(
+    df["rating_weighted_average"] = df.apply(
         lambda x: (
             (x[count_col] / (x[count_col] + m) * x[avg_rating_col])
             + (m / (m + x[count_col]) * C)
