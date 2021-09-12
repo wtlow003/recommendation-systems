94a73e4ef06f0dc9359106624a43c2a3499b9209
wtlow003 <wtlow003@suss.edu.sg>
2021-09-13 03:31:40 +0800

    add: :sparkles: generate user-item paragraph vector representation model

--- projects/product-reviews-recommender/src/generate_vectors.py
+++ projects/product-reviews-recommender/src/generate_vectors.py
@@ -3,10 +3,10 @@ import os
 import sys
 
 import click
-from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 import pandas as pd
-from tqdm import tqdm
 import yaml
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from tqdm import tqdm
 
 
 @click.command()
@@ -35,19 +35,44 @@ def main(input_filepath: str, output_filepath: str):
         lambda x: x.split()
     )
 
-    # creating tagged documents
-    train_corpus = [
+    # creating tagged documents for item representation D2V
+    item_review_corpus = [
         TaggedDocument(review, [asin])
         for asin, review in list(zip(train["asin"], train["processedReviewText"]))
     ]
+    # creating tagged documents for user-item representation D2V
+    user_review_corpus = [
+        TaggedDocument(review, [reviewerID])
+        for reviewerID, review in list(
+            zip(train["reviewerID"], train["processedReviewText"])
+        )
+    ]
+    user_item_review_corpus = item_review_corpus + user_review_corpus
 
-    model = Doc2Vec(**MODEL_PARAMS)
-    model.build_vocab(train_corpus)
-    model.train(train_corpus, total_examples=model.corpus_count, epochs=model.epochs)
+    # train item only representation in F-vector dimension space
+    item_d2v = Doc2Vec(**MODEL_PARAMS)
+    item_d2v.build_vocab(item_review_corpus)
+    item_d2v.train(
+        item_review_corpus, total_examples=item_d2v.corpus_count, epochs=item_d2v.epochs
+    )
+
+    # train user and item representation in F-vector dimension space
+    user_item_d2v = Doc2Vec(**MODEL_PARAMS)
+    user_item_d2v.build_vocab(user_item_review_corpus)
+    user_item_d2v.train(
+        user_item_review_corpus,
+        total_examples=user_item_d2v.corpus_count,
+        epochs=user_item_d2v.epochs,
+    )
 
     # saving model
     os.makedirs(output_filepath, exist_ok=True)
-    model.save(f"{output_filepath}/{CATEGORY}_{MODEL_PARAMS['vector_size']}_d2v.model")
+    item_d2v.save(
+        f"{output_filepath}/{CATEGORY}_item_{MODEL_PARAMS['vector_size']}_{MODEL_PARAMS['epochs']}_d2v.model"
+    )
+    user_item_d2v.save(
+        f"{output_filepath}/{CATEGORY}_user_item_{MODEL_PARAMS['vector_size']}_{MODEL_PARAMS['epochs']}_d2v.model"
+    )
 
 
 if __name__ == "__main__":
