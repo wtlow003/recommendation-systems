82afd274fa73e71972c89014fbfb63fa13dddaef
wtlow003 <wtlow003@suss.edu.sg>
2021-09-24 00:56:16 +0800

    add: :sparkles: algorithm for querying

--- projects/product-reviews-recommender/app.py
+++ projects/product-reviews-recommender/app.py
@@ -2,55 +2,64 @@ import pandas as pd
 import streamlit as st
 from sqlalchemy import create_engine
 
-DATA_PATH = "data/evaluation"
 
-st.title(
-    "Leveraging Unsupervised Representation Learning with Reviews for Top-N Recommendations in E-commerce"
-)
+def main():
+    """ """
+    DATA_PATH = "data/evaluation"
 
-conn = create_engine("sqlite:///recommender.db", echo=False)
+    st.title(
+        "Leveraging Unsupervised Representation Learning with Reviews for Top-N Recommendations in E-commerce"
+    )
 
-# receive `reviewerID` to get recommendations
-st.subheader("Enter Reviewer ID:")
-st.text_input("", key="reviewerID")
+    conn = create_engine("sqlite:///recommender.db", echo=False)
 
-# select category
-st.subheader("Select Category for Recommendations:")
-category_option = st.selectbox("", ("Grocery and Gourmet Food", "Pet Supplies"))
+    # receive `reviewerID` to get recommendations
+    st.subheader("Enter Reviewer ID:")
+    st.text_input("", key="reviewerID")
 
-# load data
-CATEGORY = "_".join(category_option.split(" "))
-DATA = pd.read_csv(f"{DATA_PATH}/{CATEGORY}_train.csv")
+    # select category
+    st.subheader("Select Category for Recommendations:")
+    category_option = st.selectbox("", ("Grocery and Gourmet Food", "Pet Supplies"))
 
-# select algorithm for recommendations
-st.subheader("Select Recommendation Algorithm:")
-algo_option = st.selectbox(
-    "",
-    ("UB-CF", "RANDOM", "ER-CBF"),
-)
+    # load data
+    CATEGORY = "_".join(category_option.split(" "))
+    DATA = pd.read_csv(f"{DATA_PATH}/{CATEGORY}_train.csv")
 
-# top-n items for recommendations
-st.subheader("Select N-items Recommended:")
-n_option = st.selectbox("", (10, 25, 30, 45))
+    # select algorithm for recommendations
+    st.subheader("Select Algorithm for Recommendations:")
+    algo_option = st.selectbox(
+        "",
+        ("UB-CF", "RANDOM", "ER-CBF", "FUNK-SVD", "MOD-ECF"),
+    )
 
-st.write(" ")
+    # top-n items for recommendations
+    st.subheader("Select N-items Recommended:")
+    n_option = st.selectbox("", (10, 25, 30, 45))
 
-with st.container():
-    st.header("Generating Recommendations:")
-    st.subheader(f"{st.session_state.reviewerID}'s past purchase history")
-    with st.spinner("Identifying past items"):
-        purchase_history = DATA[DATA["reviewerID"] == st.session_state.reviewerID]
-        st.table(purchase_history[["asin", "title"]])
-        st.subheader(f"{st.session_state.reviewerID} Top-{n_option} Recommended Items:")
-        n_recommended = pd.read_sql(
-            f"""SELECT *
-                FROM {CATEGORY}
-                WHERE reviewerID = '{st.session_state.reviewerID}'
-                    AND algorithm = '{algo_option}'
-                ORDER BY item_rank
-                LIMIT {n_option}
-            """,
-            con=conn,
-        )
-        st.table(n_recommended[["asin", "title"]])
-    st.success("Done!")
+    st.write(" ")
+
+    with st.container():
+        st.header("Generating Recommendations:")
+        st.subheader(f"{st.session_state.reviewerID}'s Past Purchase History")
+        with st.spinner("Identifying past items"):
+            purchase_history = DATA[DATA["reviewerID"] == st.session_state.reviewerID]
+            st.table(purchase_history[["asin", "title"]])
+            st.subheader(
+                f"{st.session_state.reviewerID} Top-{n_option} Recommended Items:"
+            )
+            n_recommended = pd.read_sql(
+                f"""SELECT *
+                    FROM {CATEGORY}
+                    WHERE reviewerID = '{st.session_state.reviewerID}'
+                        AND algorithm = '{algo_option}'
+                    ORDER BY item_rank
+                    LIMIT {n_option}
+                """,
+                con=conn,
+            )
+            st.table(n_recommended[["asin", "title"]])
+        st.success("Done!")
+
+
+if __name__ == "__main__":
+    main()
