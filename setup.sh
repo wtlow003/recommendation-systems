b58f815c29f0dfa64110fa978f783745702a4be2
wtlow003 <wtlow003@suss.edu.sg>
2021-10-23 18:12:48 +0800

    thrash: :fire: heroku materials

--- setup.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p ~/.streamlit/
-
-echo "[server]"  > ~/.streamlit/config.toml
-echo "headless = true"  >> ~/.streamlit/config.toml
-echo "port = $PORT"  >> ~/.streamlit/config.toml
-echo "enableCORS = false"  >> ~/.streamlit/config.toml
